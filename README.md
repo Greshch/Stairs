af4feb68d5aa5d325c4857891e0f87a14486ba33
Greshch <greshchook@gmail.com>
2022-04-23 08:42:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Solve of stairs task.
---
+++ Stairs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stairs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input quantity of stairs: ");
+            int n = Convert.ToInt32(System.Console.ReadLine());
+            DrawStairs(n);
+        }
+
+        public static void DrawStairs(int steps)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                int spaces = 2 * i;
+                for (int j = 0; j < spaces; j++)
+                {
+                    Console.Write(' ');
+                }
+                Console.WriteLine("***");
+                spaces = 2 * (i + 1);
+                for (int j = 0; j < spaces; j++)
+                {
+                    Console.Write(' ');
+                }
+                Console.WriteLine("*");
+            }
+            
+        }
+    }
+}
